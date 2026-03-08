ea1a671e136f11c2022ebc70f02a4331b817ac5c
J101R <90167416+J101R@users.noreply.github.com>
2026-03-07 20:00:22 -0600

    chore: fully implement sandbox hardening suggestion -clean up most environment variables -added a bind mount to deal with systemd-resolved -added FAKEROOTDONTTRYCHOWN to resolve issues between bubblewrap and chroot

--- res/wrapper/security-wrapper.sh
+++ res/wrapper/security-wrapper.sh
@@ -34,8 +34,24 @@ exec nice -n19 \
   --tmpfs /var/tmp \
   --perms 0700 --tmpfs "$XDG_RUNTIME_DIR" \
   --tmpfs "$HOME" \
+  --ro-bind /etc/resolv.conf /etc/resolv.conf \
   --ro-bind-try "${GNUPGHOME:-$HOME/.gnupg}/pubring.kbx" "${GNUPGHOME:-$HOME/.gnupg}/pubring.kbx" \
   --ro-bind-try "${GNUPGHOME:-$HOME/.gnupg}/pubring.gpg" "${GNUPGHOME:-$HOME/.gnupg}/pubring.gpg" \
+  --clearenv \
+  --setenv XDG_RUNTIME_DIR "$XDG_RUNTIME_DIR" \
+  --setenv PATH "$PATH" \
+  --setenv USER "$USER" \
+  --setenv LOGNAME "$LOGNAME" \
+  --setenv TERM "$TERM" \
+  --setenv HOME "$HOME" \
+  --setenv GNUPGHOME "${GNUPGHOME:-$HOME/.gnupg}" \
+  --setenv LANG "$LANG" \
+  --setenv PKGDEST "$PKGDEST" \
+  --setenv SRCDEST "$SRCDEST" \
+  --setenv SRCPKGDEST "$SRCPKGDEST" \
+  --setenv LOGDEST "$LOGDEST" \
+  --setenv BUILDDIR "$BUILDDIR" \
+  --setenv FAKEROOTDONTTRYCHOWN 1 \
   "${wrap_args[@]}" \
   --seccomp 3 3< "$RUA_SECCOMP_FILE" \
   "$@"
